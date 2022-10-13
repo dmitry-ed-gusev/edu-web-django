c285e9742f300c39a421961c1f06e00596c0478c
Dmitrii Gusev <dmitrii.gusev@broadcom.com>
2022-10-13 18:47:37 +0200

    Week #4 (ADS app #3). Finished, need to be tested.

--- ads/urls.py
+++ ads/urls.py
@@ -25,6 +25,10 @@ urlpatterns = [
     path('comment/<int:pk>/delete', views.CommentDeleteView.as_view(success_url=reverse_lazy('ads')),
          name='ad_comment_delete'),
 
+    # favorite / un-favorite
+    path('ad/<int:pk>/favorite', views.AddFavoriteView.as_view(), name='ad_favorite'),
+    path('ad/<int:pk>/unfavorite', views.DeleteFavoriteView.as_view(), name='ad_unfavorite'),
+
 ]
 
 # We use reverse_lazy in urls.py to delay looking up the view until all the paths are defined
--- ads/views.py
+++ ads/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from ads.models import Ad, Comment
+from ads.models import Ad, Comment, Fav
 from ads.forms import CreateForm, CommentForm
 from ads.owner import OwnerListView, OwnerDetailView, OwnerDeleteView, OwnerUpdateView, OwnerCreateView
 
@@ -22,7 +22,23 @@ def autoview(request):
 class AdListView(OwnerListView):
     model = Ad
     # By convention:
-    # template_name = "ads/ad_list.html"
+    template_name = "ads/ad_list.html"
+
+    # todo: remove!
+    # model = Thing
+    # template_name = "favs/list.html"
+
+    def get(self, request):
+        ad_list = Ad.objects.all()
+        favorites = list()
+        if request.user.is_authenticated:
+            # rows = [{'id': 2}, {'id': 4} ... ]  (A list of rows)
+            rows = request.user.favorite_ads.values('id')
+            # favorites = [2, 4, ...] using list comprehension
+            favorites = [row['id'] for row in rows]
+        ctx = {'ad_list': ad_list, 'favorites': favorites}
+
+        return render(request, self.template_name, ctx)
 
 
 class AdDetailView(OwnerDetailView):
@@ -117,3 +133,36 @@ class CommentDeleteView(OwnerDeleteView):
     def get_success_url(self):
         ad = self.object.ad
         return reverse('ads:ad_detail', args=[ad.id])
+
+
+# csrf exemption in class based views
+# https://stackoverflow.com/questions/16458166/how-to-disable-djangos-csrf-validation
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.db.utils import IntegrityError
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class AddFavoriteView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        print("Add PK", pk)
+        t = get_object_or_404(Ad, id=pk)
+        fav = Fav(user=request.user, ad=t)
+        try:
+            fav.save()  # In case of duplicate key
+        except IntegrityError as e:
+            pass
+        return HttpResponse()
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class DeleteFavoriteView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        print("Delete PK", pk)
+        t = get_object_or_404(Ad, id=pk)
+        try:
+            fav = Fav.objects.get(user=request.user, ad=t).delete()
+        except Fav.DoesNotExist as e:
+            pass
+
+        return HttpResponse()
